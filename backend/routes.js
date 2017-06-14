aa85a172f1672ee8e86df33eaea6465163d152a7
jairo Rudas <jairorudasp@gmail.com>
2017-06-14 18:54:16 -0300

    Routes criada.
    tratamento de rotas criado no backend.

--- backend/routes.js
+++ backend/routes.js
@@ -1,1 +1,12 @@
-routes.js
+const express = require('express')
+
+module.exports = () => {
+  //API  routes
+
+  const router = express.Router();
+  server.use('/api', router)
+
+  //TODO Router
+  const todoService = require('../api/todo/todoService')
+  todoService.register(router, '/todos')
+}
